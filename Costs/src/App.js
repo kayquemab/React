bb9f6f9236c46b7665bfe24cf9df4399c7191caa
Kayque Miqueias <kayquemiqueias17@gmail.com>
2025-07-21 22:44:42 -0300

    React #18

--- Costs/src/App.js
+++ Costs/src/App.js
@@ -1,8 +1,42 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <>Costs</>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/Company">Sobre</Link>
+        <Link to="/Contact">Contato</Link>
+        <Link to="/NewProject">Novo Projeto</Link>
+      </div>
+
+      <Container customClass="min-height">
+        <Routes>
+
+          {/* Home */}
+          <Route path="/" element={<Home />} />
+
+          {/* Company */}
+          <Route path="/Company" element={<Company />} />
+
+          {/* Contact */}
+          <Route path="/Contact" element={<Contact />} />
+
+          {/* New Project */}
+          <Route path="/NewProject" element={<NewProject />} />
+
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
---
+++ Costs/src/components/pages/NewProject.js
@@ -0,0 +1,9 @@
+function NewProject() {
+  return (
+    <div>
+      <h1>New</h1>
+    </div>
+  );
+}
+
+export default NewProject;
