7feb0527fb2264d2d1cb66de52e3cbb188f32178
Kayquemab <kayquemiqueias17@gmail.com>
2025-07-25 11:36:12 -0300

    React #20

--- Costs/src/components/pages/Home.js
+++ Costs/src/components/pages/Home.js
@@ -1,8 +1,16 @@
+import styles from './Home.module.css';
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton';
+
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <section className={styles.home}>
+      <h1>Bem-vindo ao <span>Costs</span></h1>
+      <p>Começe a gerenciar seus projetos agora mesmo!</p>
+
+      <LinkButton to="/newproject" text="Criar Projeto"/>
+      <img src={savings} alt='Costs'/>
+    </section>
   );
 }
 
