606dc2194e5510adef2bf494c82e52257f89c5b6
Kayque Miqueias <kayquemiqueias17@gmail.com>
2025-07-28 11:53:05 -0300

    React #21

--- Costs/src/components/pages/NewProject.js
+++ Costs/src/components/pages/NewProject.js
@@ -1,7 +1,13 @@
+import ProjectForm from '../project/ProjectForm';
+import styles from './NewProject.module.css';
+
 function NewProject() {
   return (
-    <div>
-      <h1>New</h1>
+    <div className={styles.newProject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar o serviço</p>
+      
+      <ProjectForm/>
     </div>
   );
 }
---
+++ Costs/src/components/project/ProjectForm.js
@@ -0,0 +1,29 @@
+function ProjectForm() {
+    return (
+
+        <form>
+
+            <div>
+                <input type="text" placeholder="insira o nome do projeto" />
+            </div>
+
+            <div>
+                <input type="number" placeholder="insira o orçamento total" />
+            </div>
+
+            <div>
+                <select name="category_id">
+                    <option disabled> Selecione a Categoria: </option>
+                </select>
+            </div>
+
+            <div>
+                <input type="submit" value="Criar Projeto"/>
+            </div>
+
+        </form>
+
+    )
+}
+
+export default ProjectForm
