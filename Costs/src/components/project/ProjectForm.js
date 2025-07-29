0978952b13500b604ed660bf704cfbb9374e0572
Kayquemab <kayquemiqueias17@gmail.com>
2025-07-29 14:37:04 -0300

    React #23

--- Costs/src/components/project/ProjectForm.js
+++ Costs/src/components/project/ProjectForm.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 
 import Input from '../form/Input'
 import Select from '../form/Select'
@@ -5,6 +6,22 @@ import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({ bntText }) {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/caregories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
     return (
 
         <form className={styles.form}>
@@ -26,6 +43,7 @@ function ProjectForm({ bntText }) {
             <Select
                 name="category_id"
                 text="Selecione a Categoria"
+                options={categories}
             />
 
             <SubmitButton text={bntText} />
